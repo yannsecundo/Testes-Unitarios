3c1d8d0104e5006b7c676f8f0020f25a421726fe
yannsecundo <yannsecundo34@gmail.com>
2022-05-18 15:08:05 -0400

    correção requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,11 +17,11 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome === undefined || idade === undefined) return undefined;
-  return `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+    if (nome === undefined || idade === undefined) return undefined;
+return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
